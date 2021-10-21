a9606dac57029a400e6086741e72c1630b6cb69b
brentempasisx <brentempasisx3@gmail.com>
2021-10-21 11:29:11 +0800

    ignored venv folder

--- venv/Lib/site-packages/DiscordUtils/__init__.py
+++
@@ -1,8 +0,0 @@
-from DiscordUtils import Pagination
-from DiscordUtils.InviteTracker import InviteTracker
-from DiscordUtils.Music import Music, EmptyQueue, NotConnectedToVoice, NotPlaying
-
-__title__ = "DiscordUtils"
-__version__ = "1.3.4"
-__author__ = "toxicrecker"
-__license__ = "MIT"
